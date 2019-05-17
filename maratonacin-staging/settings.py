2e4c4e8aee81ac6143f168ce1ae7f3a1b67ca152
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-17 15:25:43 -0300

    Added Banner

--- maratonacin-staging/settings.py
+++ maratonacin-staging/settings.py
@@ -111,12 +111,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Django Sass
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'assets'),
+    # os.path.join(BASE_DIR, 'media'),
     os.path.join(BASE_DIR, 'static'),
 ]
 
@@ -126,10 +127,10 @@ STATICFILES_FINDERS = [
 ]
 
 # Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-import django_heroku
+# import django_heroku
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
