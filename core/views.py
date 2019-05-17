fabea82892afd3e4c4c6696356d501d4e9f7ada0
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-16 23:09:22 -0300

    Integrate admin model to template

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Post
 
-class HomeView(TemplateView):
-    template_name = "index.html"
+def index(request):
+    return render(request, 'index.html', {'posts':Post.objects.all()})
--- maratonaCin/urls.py
+++ maratonaCin/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import HomeView
+import core.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home')
+    path('', core.views.index, name='index')
 ]
