52a56f0959ef08d20e43019b85715f13cac0b3a4
José Bruno <jbfo@cin.ufpe.br>
2019-05-18 12:07:04 -0300

    Edição do arquivo settings.py - Allowed Hosts e caminho do diretório .css

--- maratonacin-staging/settings.py
+++ maratonacin-staging/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = '*%1)_4js25^cyo(*ybh7y*5m9+jyxuclgno=9q!307&j0rij_p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',  'maratona-cin.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'core/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Django Sass
