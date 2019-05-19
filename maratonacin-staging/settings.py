9269dd74ef52b1b4b8b66106d4f6b8466b2f047e
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-19 20:34:18 -0300

    Fix static and media dirs location

--- maratonacin-staging/settings.py
+++ maratonacin-staging/settings.py
@@ -110,21 +110,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-STATIC_URL = '  /static/'
-
-# Django Sass
-SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'sass_processor.finders.CssFinder',
 ]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Django Sass
+
+SASS_PROCESSOR_INCLUDE_DIRS = [
+    os.path.join(BASE_DIR, 'extra-styles/scss'),
+    os.path.join(BASE_DIR, 'node_modules'),
+]
+
+# print(SASS_PROCESSOR_ROOT)
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
 
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
