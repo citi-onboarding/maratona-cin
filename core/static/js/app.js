ea871338783a830d821c341193fe50b94795da1d
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-19 07:10:07 -0300

    Add text container

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -2,9 +2,12 @@ $(document).ready(() => {
   $('.carousel').slick({
     slidesToShow: 1,
     dots: true,
+    // variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
     customPaging: function (slider, i) {
       return '<div class="dot" id=' + i + "></div>";
     },
-    arrows : false,
+    // arrows : false,
   });
 });
