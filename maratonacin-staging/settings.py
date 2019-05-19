dd52f11cec92152b57e1a2fcfae32bb6e6a59002
José Bruno <jbfo@cin.ufpe.br>
2019-05-19 02:01:50 -0300

    Navbar in progress

--- maratonacin-staging/settings.py
+++ maratonacin-staging/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-STATIC_URL = '/static/'
+STATIC_URL = '  /static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Django Sass
