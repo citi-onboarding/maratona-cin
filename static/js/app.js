76f5fdf2a1db3b440d61b69e8fbd0e008c2dfe0e
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-20 00:34:05 -0300

    Added Card Base

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,41 @@
-$(document).ready(() => {
-  $('.carousel').slick({
-    slidesToShow: 1,
-    dots: true,
-    // variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    customPaging: function (slider, i) {
-      return '<div class="dot" id=' + i + "></div>";
+$('.carousel').slick({
+  slidesToShow: 1,
+  dots: true,
+  // variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  customPaging: function (slider, i) {
+    return '<div class="dot" id=' + i + "></div>";
+  },
+  arrows: false,
+})
+
+$('.carousel-cards').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1008,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
     },
-    arrows : false,
-  });
+    {
+      breakpoint: 600,
+      settings: 'unslick',
+    },
+  ]
+});
+
+$(window).resize(() => {
+  $('.js-slider').not('.slick-initialized').slick('resize');
+});
+
+$(window).on('orientationchange', () => {
+  $('.js-slider').not('.slick-initialized').slick('resize');
 });
