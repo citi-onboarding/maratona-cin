ea1e3424460ec0704ffe6872f63353444c086c9e
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-22 10:06:57 -0300

    Add Responsive Carousel

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+// About section
 $('.carousel-about').slick({
   slidesToShow: 1,
   dots: true,
@@ -5,8 +6,9 @@ $('.carousel-about').slick({
   autoplaySpeed: 2000,
   customPaging: (slider, i) => `<div class="dot" id=${i}></div>`,
   arrows: false,
-})
+});
 
+// Cards section
 const addSlick = () => {
   let carousel = $('.carousel-cards');
   if (window.innerWidth < 960) {
@@ -21,15 +23,16 @@ const addSlick = () => {
   } else if (carousel.slick){
     carousel.slick('unslick');
   }
-}
+};
 
 window.addEventListener('resize', addSlick);
 window.addEventListener('load', addSlick);
 
+// Team section
 $('.carousel-team').slick({
-  slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToShow: 1,
+  slidesToScroll: 1,
   dots: false,
   prevArrow: '<a class="prev arrow fa fa-angle-left"></a>',
   nextArrow: '<a class="next arrow fa fa-angle-right"></a>',
-})
+});
