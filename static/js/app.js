38fbe0cb7368e32c19a0ced681badc89415249d4
José Bruno <jbfo@cin.ufpe.br>
2019-05-23 15:59:51 -0300

    Added navbar fixed functionality

--- static/js/app.js
+++ static/js/app.js
@@ -36,3 +36,35 @@ $('.carousel-team').slick({
   prevArrow: '<a class="prev arrow fa fa-angle-left"></a>',
   nextArrow: '<a class="next arrow fa fa-angle-right"></a>',
 });
+
+// Navbar
+
+const getHeight = element => document.querySelector(element).clientHeight;
+
+let heights = {
+  navbar: getHeight('.navbar'),
+  banner: getHeight('.banner'),
+  // information: getHeight('#information') + 40,
+  // journey: getHeight('#journey'),
+  // testimonials: getHeight('.testimonials-news'),
+  // sucess: getHeight('.results'),
+  // partners: getHeight('#partners')
+}
+
+const navbar = document.querySelector('.navbar');
+const menu = document.querySelector('.menu-container');
+
+document.querySelector('.show-menu').addEventListener('click', () => {
+  menu.clientHeight === 0 ?  menu.style.height = '200px' : menu.style.height = '0px' ;
+})
+
+document.addEventListener('scroll', event => {
+  if (window.scrollY >= heights.banner) {
+    if (navbar.className.indexOf('fixed') == -1) {
+      navbar.className += ' fixed';
+    }
+    console.log(navbar.className);
+  } else {
+    navbar.className = navbar.className.split('fixed').join('');
+  }
+})
