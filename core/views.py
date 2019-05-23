09ff41fb11bdc347892008fb5bd96e9f24ff488c
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-22 23:55:23 -0300

    Added forloop to participant-container

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Participant
+import math
 
 def index(request):
-    return render(request, 'index.html', {'participants':Participant.objects.all()})
+    return render(request, 'index.html', {
+      'participants':Participant.objects.all(),
+      'containers_num':math.ceil(len(Participant.objects.all())/4)
+      })
--- maratonacin-staging/settings.py
+++ maratonacin-staging/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'sass_processor',
+    'mathfilters',
 ]
 
 MIDDLEWARE = [
