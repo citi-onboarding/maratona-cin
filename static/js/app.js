9aac5d8df768495a5673ecfc494d011c1f848670
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-29 18:58:51 -0300

    Single Message

--- static/js/app.js
+++ static/js/app.js
@@ -189,19 +189,6 @@ $('.carousel-testimonials').slick({
   pauseOnHover: false,
 });
 
-// Set height
-// let maxHeight = -1;
-// $('.slick-slide').each(function () {
-//   if ($(this).height() > maxHeight) {
-//     maxHeight = $(this).height();
-//   }
-// });
-// $('.slick-slide').each(function () {
-//   if ($(this).height() < maxHeight) {
-//     $(this).css('margin', Math.ceil((maxHeight - $(this).height()) / 2) - 170 + 'px 0');
-//   }
-// });
-
 // Organize by colors
 
 const invert = subj => {
