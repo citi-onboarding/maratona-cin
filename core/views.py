b65bac9592b3397a760e919be10739367830a5c1
Bruno <jbfo@cin.ufpe.br>
2019-06-02 12:11:02 -0300

    Leaderboards feature updated. Image is still pending

--- core/views.py
+++ core/views.py
@@ -8,8 +8,43 @@ import math
 
 
 def index(request):
+
+  k = 0
+  nacional_list = list(Medal.objects.all())
+  nacional_sublist = list()
+  slide_nacional = []
+  for medal in nacional_list:
+    if medal.type == 'Nacional':
+      if (k < 1):
+        slide_nacional.append(medal)
+        k += 1
+      else:
+        slide_nacional.append(medal)
+        nacional_sublist.append(slide_nacional)
+        slide_nacional = []
+        k = 0
+  nacional_sublist.append(slide_nacional)
+
+  l = 0
+  internacional_list = list(Medal.objects.all())
+  internacional_sublist = list()
+  slide_internacional = []
+  for medal in internacional_list:
+    if medal.type == 'Internacional':
+      if (l < 2):
+        slide_internacional.append(medal)
+        l += 1
+      else:
+        slide_internacional.append(medal)
+        internacional_sublist.append(slide_internacional)
+        slide_internacional = []
+        l = 0
+  internacional_sublist.append(slide_internacional)
+
   return render(request, 'index.html', {
     'Medal': Medal.objects.all().order_by('type'),
+    'nacional_list': nacional_sublist,
+    'internacional_list': internacional_sublist,
     'Schedule': Schedule.objects.all().order_by('activity_date'),
     'participants':Participant.objects.all(),
     'containers_num':math.ceil(len(Participant.objects.all())/4)
