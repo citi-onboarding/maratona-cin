a665a45a914d3d9a8aa555b2dbb1a3de0a3c88a2
Romildo Júnior <jrmmendes@outlook.com>
2019-07-23 12:53:04 -0300

    Hotfix: migrations

--- maratonacin-staging/settings.py
+++ maratonacin-staging/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'core.apps.CoreConfig',
     'sass_processor',
     'mathfilters',
     'storages',
